6fd66aabd2a11457efceb95af81c4d60b4c08632
balwant3186 <34373891+balwant3186@users.noreply.github.com>
2019-06-09 20:16:48 +0530

    Add files via upload

---
+++ day_2_p_3.py
@@ -0,0 +1,6 @@
+#Volume of box
+
+l = int(input("Enter length: "))
+b = int(input("Enter breadth: "))
+h = int(input("Enter height: "))
+print("Volume of box is: ", l * b * h)
---
+++ day_2_p_4.py
@@ -0,0 +1,7 @@
+#Volume of earth
+
+import math
+
+r = float(input("Enter the radius: "))
+volume = 4/3 * math.pi * (r ** 3)
+print("Volume of earth is ", volume, "cubic metres")
