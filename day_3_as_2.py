b75425106c0fa2fde6dd4716c3122f655090a3a5
balwant3186 <34373891+balwant3186@users.noreply.github.com>
2019-06-12 07:34:03 +0530

    Update day_3_as_2.py

--- day_3_as_2.py
+++ day_3_as_2.py
@@ -1,16 +1,16 @@
-def twoCharUpward(char, n):
+def twoCharUpward(c, n):
     for i in range(0, n, 2):
-        print(((n-i)//2)*" ", char * i, ((n-i)//2)*" ")
+        print(((n-i)//2)*" ", c * i, ((n-i)//2)*" ")
     return
 
-def twoCharDownward(char, n):
+def twoCharDownward(c, n):
     for i in range(n, 0, -2):
-        print(((n-i)//2)*" ", char * i, ((n-i)//2)*" ")
+        print(((n-i)//2)*" ", c * i, ((n-i)//2)*" ")
     return
 
-char = input("Enter a character: ")
-n = int(input("Enter a incremental series limit: "))
-m = int(input("Enter a decremental series limit: "))
+c = input("Enter a character: ")
+n = int(input("Enter a limit: "))
+m = int(input("Enter a limit: "))
 
-twoCharUpward(char, n)
-twoCharDownward(char, m)
+twoCharUpward(c, n)
+twoCharDownward(c, m)
