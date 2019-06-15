fcc6bd426d055139885117a1b378c32cb603e751
balwant3186 <34373891+balwant3186@users.noreply.github.com>
2019-06-15 18:54:45 +0530

    Delete day_3_p_9.py

--- day_3_p_9.py
+++
@@ -1,7 +0,0 @@
-def add(num1, num2):
-    res = num1 + num2
-    print(num1, "+", num2, "=", res)
-    return None
-num1 = int(input("Enter first number:"))
-num2 = int(input("Enter second number:"))
-add(num1, num2)
