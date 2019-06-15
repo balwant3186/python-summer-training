9087a422f2dd36101a90e61264a2ea1dcb2cbec6
balwant3186 <34373891+balwant3186@users.noreply.github.com>
2019-06-15 18:53:32 +0530

    Delete day_3_p_4.py

--- day_3_p_4.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number:"))
-
-if num % 2 == 0:
-    print(num, "is even")
-    print("If block executed")
-
-else:
-    print(num, "is odd")
-    print("Else block executed")
