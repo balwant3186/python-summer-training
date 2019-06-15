945486bc0fd56aaad8d3d61ee4e4cad40888f8ea
balwant3186 <34373891+balwant3186@users.noreply.github.com>
2019-06-15 18:56:27 +0530

    Delete day_4_p_3.py

--- day_4_p_3.py
+++
@@ -1,9 +0,0 @@
-fo = open(r'C:\Python\Python Summer Training VIPS\hello.txt', 'w')
-str1 = input('Enter text: ')
-
-x = fo.write(str1)
-print('No. of bytes written: ', x)
-fo.write('\nWe are using file handling')
-fo.close()
-
-print('Work done')
