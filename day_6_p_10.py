26a8481e3ee9a939df19edea5a458c652decb745
balwant3186 <34373891+balwant3186@users.noreply.github.com>
2019-06-15 18:57:54 +0530

    Delete day_6_p_10.py

--- day_6_p_10.py
+++
@@ -1,8 +0,0 @@
-items = ['egg', 'milk', 'butter', 'peanuts', 'oats', 'honey']
-for i in enumerate(items):
-    print(i, end = ',')
-print('a')
-items = ['cup', 'pen', 'book']
-
-for i in enumerate(items, 100):
-    print(i, end = ',')
