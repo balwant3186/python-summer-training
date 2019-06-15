f39b1c95288a7948c7f3b6c68f31ad28676203ba
balwant3186 <34373891+balwant3186@users.noreply.github.com>
2019-06-15 18:56:12 +0530

    Delete day_4_p_2.py

--- day_4_p_2.py
+++
@@ -1,7 +0,0 @@
-fo = open(r'C:\Python\Python Summer Training VIPS\hello.txt', 'w')
-print('Name of file: ', fo.name)
-print('Closed or not? ', fo.closed)
-print('Opening mode: ', fo.mode)
-
-fo.close()
-print('Closed or not? ', fo.closed)
